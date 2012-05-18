4bc25cf1a10eb6c9b4978960a79e197b2e34ac3b
tidjean <tidjean@gmail.com>
2012-05-18 11:26:32 +0800

    list country

--- includes/languages/french/checkout_process.php
+++ includes/languages/french/checkout_process.php
@@ -12,8 +12,8 @@
 
 define('EMAIL_SEPARATOR', '------------------------------------------------------');
 define('EMAIL_TEXT_SUBJECT', 'Traitement de la commande');
-define('EMAIL_TEXT_ORDER_NUMBER', 'Les vacances approchent, nous vous invitons à suivre l\'état de livraison de votre colis via le tracking mentionné sur www.bpostinternational.com, en cas d\'absence, le colis reste en poste pendant une période aléatoire de 5 à 15 jours, au dela de ce délai, votre colis nous sera retourné.<br />
-Vous pouvez donner procuration à un ami ou à un voisin avec la copie de votre pièce d\'identité pour éviter que votre colis nous soit retourné.<br />
+define('EMAIL_TEXT_ORDER_NUMBER', 'Les vacances approchent, nous vous invitons &agrave; suivre l\'état de livraison de votre colis via le tracking mentionné sur www.bpostinternational.com, en cas d\'absence, le colis reste en poste pendant une période aléatoire de 5 &agrave; 15 jours, au dela de ce délai, votre colis nous sera retourné.<br />
+Vous pouvez donner procuration &agrave; un ami ou &agrave; un voisin avec la copie de votre pièce d\'identité pour éviter que votre colis nous soit retourné.<br />
 '.EMAIL_SEPARATOR."\n".'Numéro de commande :');
 
 define('EMAIL_TEXT_NAME','Nom');
@@ -41,8 +41,8 @@ define('EMAIL_TEXT_CONTENT', 'Votre commande nous a été correctement envoyée.
 <br><br>
 Si votre commande ne comporte aucune anomalie, vos produits vous seront livrés dans 12 jours.
 <br /><br />
-Les vacances approchent, nous vous invitons à suivre l\'état de livraison de votre colis via le tracking mentionné sur www.bpostinternational.com, en cas d absence, le colis reste en poste pendant une période aléatoire de 5 à 15 jours, au dela de ce délai, votre colis nous sera retourné.<br />
-Vous pouvez donner procuration à un ami ou à un voisin avec la copie de votre pièce d\'identité pour éviter que votre colis nous soit retourné.');
+Les vacances approchent, nous vous invitons &agrave; suivre l\'état de livraison de votre colis via le tracking mentionné sur www.bpostinternational.com, en cas d absence, le colis reste en poste pendant une période aléatoire de 5 &agrave; 15 jours, au dela de ce délai, votre colis nous sera retourné.<br />
+Vous pouvez donner procuration &agrave; un ami ou &agrave; un voisin avec la copie de votre pièce d\'identité pour éviter que votre colis nous soit retourné.');
 
 define('EMAIL_TEXT_CONTENT_LESS',EMAIL_TEXT_CONTENT_HIGH);
 
@@ -65,9 +65,9 @@ define('SPONSORSHIP_EMAIL_SUBJECT', 'Nouveau code de réduction '.STORE_NAME);
 
 define('SPONSORSHIP_EMAIL_TEXT', 'Bonjour ,
 
-Nous sommes heureux de vous faire parvenir le code de réduction suivant : "%s" d\'une valeur de %s valable %s mois suite à la commande de votre %sfilleul %s.
+Nous sommes heureux de vous faire parvenir le code de réduction suivant : "%s" d\'une valeur de %s valable %s mois suite &agrave; la commande de votre %sfilleul %s.
 
-Vous pouvez accéder à la liste de vos réductions en vous connectant sur notre site et en utilisant le lien suivant :
+Vous pouvez accéder &agrave; la liste de vos réductions en vous connectant sur notre site et en utilisant le lien suivant :
 
 <a target="blank" href="http://'.$_SERVER['SERVER_NAME'].'/sponsorship_discount.php">http://'.$_SERVER['SERVER_NAME'].'/sponsorship_discount.php</a>
 
@@ -83,9 +83,9 @@ define('EMAIL_TEXT_DISCOUNT_SUBJECT', '%s de réduction pour vos amis !');
 
 define('EMAIL_FRIEND_DISCOUNT', 'Bonjour %s,
 
-Suite à votre commande, vos amis peuvent gràce à vous bénéficier d\'une réduction de %s.
+Suite &agrave; votre commande, vos amis peuvent gr&agrave;ce &agrave; vous bénéficier d\'une réduction de %s.
 
-Cliquez sans plus attendre sur le lien suivant pour envoyer le bon de réduction à vos amis :
+Cliquez sans plus attendre sur le lien suivant pour envoyer le bon de réduction &agrave; vos amis :
 
 <a target="blank" href="http://'.$_SERVER['SERVER_NAME'].'/friend_discount.php?var=%s">http://'.$_SERVER['SERVER_NAME'].'/friend_discount.php?var=%s</a>
 
--- includes/languages/french/shipping_details.php
+++ includes/languages/french/shipping_details.php
@@ -13,7 +13,7 @@
 define('NAVBAR_TITLE', 'D&eacute;tails d\'exp&eacute;dition');
 define('HEADING_TITLE', 'D&eacute;tails d\'exp&eacute;dition');
 
-define('TEXT_INFORMATION', '<b>Nous livrons dans le monde entier ! </b></p>
+define('TEXT_INFORMATION', '<b>Nous livrons en France Metropolitaine, Belgique, Luxembourg, Suisse, Italie, Espagne et uk !</b></p>
 <p><b>Co&ucirc;t du transport : </b><p>Gratuit pour 3 produits, 7EUR pour 2 produits, 14EUR pour 1 produit</p>
 <p><b>D&eacute;lais de livraison :</b>
 8 &agrave; 12 jours ouvrables.</p>');
