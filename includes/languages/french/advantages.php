aac84b2f857adeab82e378e318b36825c5d65be9
tidjean <tidjean@gmail.com>
2012-06-11 17:36:46 +0800

    advantage

--- includes/languages/french/advantages.php
+++ includes/languages/french/advantages.php
@@ -1,17 +1,14 @@
 <?php
 define('NAVBAR_TITLE', 'Avantages');
 define('TEXT_INFORMATION','
-<br /><h2 class="text_rose">Op&eacute;ration Cashback</h2>
+<br /><h2 class="text_rose">Cadeau</h2>
 <span style="font-size:14px;line-height:19px;">
-En ce moment sur parfumreduction.com obtenez jusqu\'a  50 euros de r&eacute;duction sur votre commande.<br/>
-Plus vous ajoutez des produits dans votre panier, plus nous vous offrons des r&eacute;ductions importantes sur votre commande.<br/>
-<ul>
-<li>Si vous achetez 2 produits vous b&eacute;n&eacute;ficiez de 5 euros de r&eacute;duction imm&eacute;diatement</li>
-<li>Si vous achetez 3 produits vous b&eacute;n&eacute;ficiez de 18 euros de r&eacute;duction imm&eacute;diatement</li>
-<li>Si vous achetez 4 produits vous b&eacute;n&eacute;ficiez de 30  euros de r&eacute;duction imm&eacute;diatement</li>
-<li>Si vous achetez 5 produits vous b&eacute;n&eacute;ficiez de 38  euros de r&eacute;duction imm&eacute;diatement</li>
-<li>Si vous achetez 6 produits vous b&eacute;n&eacute;ficiez de 50  euros de r&eacute;duction imm&eacute;diatement</li>
-</ul>
+Pour toute commande sur le site, Parfumreduc vous offre ce Masque Collagène à l\'Or 24 carats en cadeau d\'une valeur de 39.90 euros. <br/>
+Offre  Valable jusqu\'au 20 Juin<br/>
+Ce masque innovant contient des ingrédients bioactifs, qui ont une action hydratante sur votre peau, qui réduit l\'apparition des rides. <br/>
+Ce masque à l\'or 24 carats raffermit et tonifie la peau 
+Stimule la production de collagène 
+Favorise le processus de cicatrisation de la peau et la prot&egrave;ge
 </span>
 '); 
 ?>
