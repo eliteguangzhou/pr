09554a1bad697708ccdab86f7164f7b49ea87a43
Tidjean <tidjean@gmail.com>
2012-07-10 17:37:11 +0800

    script google analytic

--- includes/header.php
+++ includes/header.php
@@ -69,6 +69,19 @@
 <script type="text/javascript" src="http://code.jquery.com/jquery-latest.js"></script>
 <script type="text/javascript" src="includes/animation.js"></script>
 <script type="text/javascript" src="includes/zoom.js"></script>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-33173893-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 <table cellpadding="0" cellspacing="0" border="0" style="width:980px" align="center">
 	<tr>
 		<td>
