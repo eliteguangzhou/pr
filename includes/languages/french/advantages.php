fe3bc8c07081d544f160f103bd79de7138f77c90
tidjean <tidjean@gmail.com>
2012-08-13 16:17:27 +0800

    advantage

--- includes/languages/french/advantages.php
+++ includes/languages/french/advantages.php
@@ -1,4 +1,7 @@
 <?php
 define('NAVBAR_TITLE', 'Avantages');
-define('TEXT_INFORMATION',''); 
+define('TEXT_INFORMATION','Parfumreduc vous chouchoute profitez donc des promos du moment.<br/>
+Actuellement pour toute commande passée sur Parfumreduc.com  obtenez 18 euros de réduction si vous achetez 3 articles.<br/>
+obtenez  30 euros de réduction si vous achetez 4 articles<br/>
+Nous vous offrons 50 euros de rabais si vous achetez 6 articles.'); 
 ?>
